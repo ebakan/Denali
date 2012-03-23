accd2dec47f9121f59bb125a41cfff8e81dfa843
Jonathon <Jonathon@Jonathons-MacBook.local>
2012-03-23 02:36:05 -0700

    Can now login with @bcp.org or without

--- loginProc.php
+++ loginProc.php
@@ -14,6 +14,11 @@ $name = mysql_real_escape_string($name);
 $pass = htmlentities($pass);
 $pass = mysql_real_escape_string($pass);
 
+if(strcmp(substr($name, -8), "@bcp.org") == 0)
+{
+    $name = substr($name, 0, -8);
+}
+
 $ldap = new LdapUser();
 
 
@@ -38,59 +43,8 @@ else{
 
 }
 
-/*if(test_login($name, $id))
-{
-    $hashkey = $name;
-    $hashkey .= SALT;
-
-    //Creates cookie
-    require_once("cookie.php");
-    $cookie = new cookie();
-    $cookie->setCookie( $hashkey, false);
-    
-    //Session data to get username and id
-    session_start();
-    $_SESSION['uname'] = $name;
-    $_SESSION['uid'] = $id;
-    header("Location: registration.php");
-}else{
-    header("Location: login.php?error=0");
-}
-*/
 
 
 
 
-   /* 
-
-        $ip = $_SERVER['REMOTE_ADDR'];
-        $browser = $_SERVER['HTTP_USER_AGENT'];
-        $sqlUser = "SELECT id FROM jelogins WHERE username = \"".$name."\"";
-        $result = mysql_query($sqlUser);
-        $resArr = mysql_fetch_array($result);
-        $id = $resArr[0];
-       
-
-        $sqlData = "INSERT INTO jelogins_data (userid, date, ip, browser)
-            VALUES('".$id."', NOW(), '".$ip."', '".$browser."')";
-
-        $result = mysql_query($sqlData) or die(mysql_error());
-        
-        //cookies
-        require_once("cookie.php");
-        $cookie = new cookie();
-        $cookie->setCookie( $id, false);
-
-        //check session
-        session_start();
-        if(isset($_SESSION['url']))
-        $location2 = $_SESSION['url'];
-        
-        echo "<script type=\"text/javascript\">\n";
-        echo "alert('Successful!');\n";
-        echo "window.location = ('$location2');\n";
-        echo "</script>";
-    }
-}
-*/
 ?>
