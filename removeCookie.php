cd12b79a3cefec4c440dac462c1e88629c612b03
Eric Bakan <eric@ebakan.com>
2012-03-23 01:35:58 -0700

    Eliminated popup in removeCookie.php

--- removeCookie.php
+++ removeCookie.php
@@ -2,11 +2,5 @@
     if(isset($_COOKIE["user"]))
     {
         setcookie("user","",time()-3600);
-	    echo "<script type=\"text/javascript\">\n";
-	    echo "alert('Logged out.');\n";
-        echo "window.location = 'login.php'";
-	    echo "</script>";
-        
-    }else
-        echo "no cookie set";
+    }
 ?>
