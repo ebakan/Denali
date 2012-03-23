358a5f71ec6d356f3d1546f0a8ac06c8c10c3d5a
Jonathon <Jonathon@Jonathons-MacBook.local>
2012-03-22 20:51:54 -0700

    fixed it again

--- config.php
+++ config.php
@@ -97,11 +97,9 @@ class system
     public function getValidEvents($id) {
         $id = mysql_escape_string($id);
         $outArray = array();
-<<<<<<< Updated upstream
         for($i = 1; $i <= 4; $i++) {
             $result = $this->getValidEventsByTimeslot($id, $i);
             if(!$result) {
->>>>>>> Stashed changes
                 return false;
             }
             $outArray[$i] = $result;
