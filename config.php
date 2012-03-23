c93b619dd5505c105b7a5dc58ce98b27f31071cb
Jonathon <Jonathon@Jonathons-MacBook.local>
2012-03-22 20:47:53 -0700

    fixed merge fo real

--- config.php
+++ config.php
@@ -98,12 +98,6 @@ class system
         $id = mysql_escape_string($id);
         $outArray = array();
 <<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        for ($i = 1; $i <= 4; $i++) {
-            $result = $this->getValidEventsByTimeslot($id, $i);
-            if (!$result) {
->>>>>>> Stashed changes
-=======
         for($i = 1; $i <= 4; $i++) {
             $result = $this->getValidEventsByTimeslot($id, $i);
             if(!$result) {
